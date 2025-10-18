b3024805996fe0a481806132a385317a7369ed73
bohdanlazurenko <142092646+bohdanlazurenko@users.noreply.github.com>
2025-10-19 01:24:55 +0200

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import DiceRoller from '../components/DiceRoller'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-4">
+      <DiceRoller />
+    </main>
+  )
+}
