0bc835054999b90956352daeff1729f3366a22c5
bohdanlazurenko <142092646+bohdanlazurenko@users.noreply.github.com>
2025-10-19 01:24:58 +0200

    Add tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 1s ease-in-out',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
